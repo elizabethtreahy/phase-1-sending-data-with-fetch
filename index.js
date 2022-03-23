40a2e4780afaf6ff9a77377a61e6091798460c80
Elizabeth <elizabethtreahy@gmail.com>
2022-03-22 21:32:47 -0400

    commit tree 780dffa26e66d9ac20016921ff9ebbfc05149d75

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Add your code here
+
+
+function submitData(userName, userEmail) {
+  const userPost = {name: userName, email: userEmail}
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(userPost)
+  })
+  .then(res => res.json())
+  .then(data => document.querySelector('body').append(data.id))
+  .catch(function (error) {
+    alert('Unauthorized Access');
+    document.querySelector('body').append(error);
+  })
+}
+
+
+
+
+
+
+
+// const formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+//   });
